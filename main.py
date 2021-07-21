bdeb56132871011beb6e0a3a6deea6fbf82384c3
lsylo <malvinlo@protonmail.com>
2021-07-21 04:18:49 -0400

    main header and pseudo structure

---
+++ main.py
@@ -0,0 +1,14 @@
+# --------------------------------------------------------------------
+# Automated Income Reports
+# Last Updated: July 2021
+# Author: lsylo
+# Status: Prototype
+# --------------------------------------------------------------------
+
+# Connect to Database
+# Extract Data
+# Build Report Visuals
+# Assemble Report
+# Export to PDF
+# Email PDF
+# Automate Weekly
